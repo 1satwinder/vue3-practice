269a0907282b4b5878ebe3fdec70fdf979202735
ssg-financial-com <satwindersinghsaini59@gmail.com>
2023-10-22 22:17:01 -0400

    emit events and props

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,23 @@
 <template>
-    <h1>In side hello world file</h1>
+    <h1>In side hello world file {{ counter }}</h1>
+    <button @click="updateCounter(10)">Increament</button>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+const props = defineProps(['counter']);
+const emit = defineEmits(['updateCounter'])
+console.log('props are', props);
+
+const counter = ref(props.counter);
+
+function updateCounter(step) {
+    counter.value += step;
+    emit('updateCounter',step);
+}
+
+onMounted(() => {
+    console.log('counter is', counter.value);
+})
 
 </script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,13 +2,22 @@
   <div class="about">
     <h1>This is an about page {{ name }} </h1>
     <button @click="changeName"> changeName </button>
+    <br><br>
+    <p>Counter Updates by child by emitting events {{ counterInRoot }}</p>
+    <HelloWorld :counter="counterInRoot" @updateCounter="updateCounter"></HelloWorld>
   </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue';
 
 const name = ref('mounted');
+const counterInRoot = ref(0);
+
+function updateCounter(increamentBy) {
+  counterInRoot.value += increamentBy;
+}
 
 onMounted(() => {
   console.log('about page is mounted');
@@ -21,5 +30,4 @@ const changeName = () => {
 
 </script>
 
-<style>
-</style>
+<style></style>
